31bf02f7644d599ae13b7606a3a4532887277cdc
TLKT0M <tom.klein@student.uni-siegen.de>
2022-04-10 09:59:45 +0200

    zeroDevision error

--- api/app.py
+++ api/app.py
@@ -45,23 +45,25 @@ def incident(land,reg,kreis,gem):
 
     incidents = Incident.query.filter(text(filterst)).all()
     #region do stats
-
+    
     count_all = len(incidents)
     statList = []
     count_IstRad =0
     count_IstPKW = 0
     count_IstFuss = 0
     count_IstKrad = 0
-    for i in incidents:
-        count_IstFuss = count_IstFuss + i.IstFuss
-        count_IstRad = count_IstRad + i.IstRad
-        count_IstKrad = count_IstKrad + i.IstKrad
-        count_IstPKW = count_IstPKW + i.IstPKW
-    statList.append(round((count_IstFuss / count_all)*100,2))
-    statList.append(round((count_IstRad / count_all)*100,2))
-    statList.append(round((count_IstKrad / count_all)*100,2))
-    statList.append(round((count_IstPKW / count_all)*100,2))
-
+    try:
+        for i in incidents:
+            count_IstFuss = count_IstFuss + i.IstFuss
+            count_IstRad = count_IstRad + i.IstRad
+            count_IstKrad = count_IstKrad + i.IstKrad
+            count_IstPKW = count_IstPKW + i.IstPKW
+        statList.append(round((count_IstFuss / count_all)*100,2))
+        statList.append(round((count_IstRad / count_all)*100,2))
+        statList.append(round((count_IstKrad / count_all)*100,2))
+        statList.append(round((count_IstPKW / count_all)*100,2))
+    except ZeroDivisionError:
+        return jsonify("Nothing Found, Please enter Valid Numbers")
     #endregion
     filtering = ["Land: "+land,"Regierung: "+reg,"Kreis: "+kreis,"Gemeinde: "+gem]
     return render_template('index.html', incidents=incidents, count_all=count_all, statList=statList, filtering=filtering )
