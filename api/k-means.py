561d3e80848ff4eb3ac18e155458fe8a3954722f
TLKT0M <tom.klein@student.uni-siegen.de>
2022-04-13 07:11:35 +0200

    bekomme es nicht ans laufen

--- api/k-means.py
+++ api/k-means.py
@@ -4,16 +4,29 @@ from scipy.spatial import distance
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.datasets import make_blobs
-
-incidents = Incident.query.filter(text(filterst)).all()
+# from crypt import methods
+import numpy as np
+from scipy.spatial import distance
+from sklearn.cluster import DBSCAN
+from matplotlib import pyplot as plt
+from distutils.log import debug
+from get_data import get_main_Df
+import json
+from sqlalchemy import text
+from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from classes.incident import Incident
+import pandas as pd
 
 def dbscan(X, eps, min_samples):
-    db = DBSCAN(eps=eps, min_samples=min_samples)
-    db.fit(X)
-    y_pred = db.fit_predict(X)
+    dbs = DBSCAN(eps=eps, min_samples=min_samples)
+    dbs.fit(X)
+    y_pred = dbs.fit_predict(X)
     plt.scatter(X[:,0], X[:,1],c=y_pred, cmap='Paired')
     plt.title("DBSCAN")
     plt.show()
 
 if __name__ == "__main__":
-    dbscan(np.array([[5.8, 2.8], [6.0, 2.2]]),0.5,1)
+    db.
+   
