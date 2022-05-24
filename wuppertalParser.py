9f36b1c893917717e97e943a68ab5eff23ee5a0d
af-code <65652602+af-code@users.noreply.github.com>
2022-05-24 22:16:32 +0200

    Json Parser for Wuppertal Data

---
+++ wuppertalParser.py
@@ -0,0 +1,57 @@
+import urllib.request, json 
+import numpy as np
+
+def get30Zones():
+    with urllib.request.urlopen("https://daten.wuppertal.de/Transport_Verkehr/Tempo30-Zonen_EPSG4326_JSON.json") as url:
+        data = json.loads(url.read().decode())
+
+    idList = {}
+    for i in range(len(data['features'])):
+        id = data['features'][i]['properties']['Id']
+        coordinates = data['features'][i]['geometry']['coordinates'][0]
+        type = data['features'][i]['geometry']['type']
+        if id in idList or type == 'MultiPolygon':
+            continue
+        idList[id] = coordinates
+
+    return idList
+
+def getSchools():
+    with urllib.request.urlopen("https://daten.wuppertal.de/Soziales/Schulen_EPSG4326_JSON.json") as url:
+        data = json.loads(url.read().decode())
+    idList = {}
+    for i in range(len(data['features'])):
+        id = data['features'][i]['properties']['ID']
+        name = data['features'][i]['properties']['Name']
+        coordinates = data['features'][i]['geometry']['coordinates']
+        idList[id] = [name, coordinates]
+
+    return idList
+
+def ray_tracing_method(x:float,y:float , poly:list):
+    n = len(poly)
+    inside = False
+
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+
+    return inside
+
+if __name__ == '__main__':
+    test = get30Zones()
+
+    # x = 7.20
+    # y = 51.27476788705908
+
+    # for i in test:
+    #     print(i)
+    #     print(ray_tracing_method(x, y, test[i]))
